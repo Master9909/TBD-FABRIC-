7dec31e829913fa34fa3973a34c81cfa42734b12
Master9909 <princealishan57@gmail.com>
2019-12-18 21:28:45 -0600

    Progress?

--- src/main/java/net/mod1/Separator.java
+++ src/main/java/net/mod1/Separator.java
@@ -4,10 +4,19 @@ import net.fabricmc.fabric.api.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tools.FabricToolTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public class Separator extends Block implements BlockEntityProvider {
 
@@ -20,6 +29,28 @@ public class Separator extends Block implements BlockEntityProvider {
     @Override
     public BlockEntity createBlockEntity(BlockView blockView) {
        return new SeparatorEntity();
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        if(blockEntity instanceof SeparatorEntity){
+            SeparatorEntity separatorEntity = (SeparatorEntity) blockEntity;
+            if(player.inventory.getCursorStack() != null){
+            if(player.inventory.getCursorStack() == new ItemStack(Items.MILK_BUCKET)){
+                if(separatorEntity.addMilk()){
+                    player.inventory.setCursorStack(new ItemStack(Items.BUCKET));
+                    return ActionResult.SUCCESS;
+                }
+            }
+            if(player.inventory.getCursorStack() == new ItemStack(Items.BUCKET)){
+                separatorEntity.removeMilk();
+                player.inventory.setCursorStack(new ItemStack(Items.MILK_BUCKET));
+                return ActionResult.SUCCESS;
+            }
+            }
+        }
+        return ActionResult.SUCCESS;
     }
 
 }
--- src/main/java/net/mod1/SeparatorEntity.java
+++ src/main/java/net/mod1/SeparatorEntity.java
@@ -1,11 +1,46 @@
 package net.mod1;
 
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundTag;
 
 public class SeparatorEntity extends BlockEntity {
 
+    private int mBucketMilk = 0;
+
+    public boolean addMilk(){
+        if (mBucketMilk == 0){
+            mBucketMilk = 1000;
+            return true;
+        }
+        return false;
+    }
+
+    public void removeMilk(){
+        if(mBucketMilk == 1000){
+            world.spawnEntity(new ItemEntity(world, pos.getX() + 0.5, pos.getY() + 1.0, pos.getZ() + 0.5, new ItemStack(Items.MILK_BUCKET)));
+            mBucketMilk = 0;
+        }
+    }
+
     public SeparatorEntity() {
         super(Mod1.SEPARATORENTITY);
+    }
+
+    @Override
+    public CompoundTag toTag(CompoundTag tag) {
+        super.toTag(tag);
+        tag.putInt("MBucketMilk", this.mBucketMilk);
+
+        return tag;
+    }
+
+    @Override
+    public void fromTag(CompoundTag tag) {
+        super.fromTag(tag);
+        this.mBucketMilk = tag.getInt("MBucketMilk");
     }
 
 }

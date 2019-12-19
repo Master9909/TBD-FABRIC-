0b6a4c22c2650e6613a0b9e558d6ed1903fa01aa
Master9909 <princealishan57@gmail.com>
2019-12-19 03:02:35 -0600

    Set up blockstate for when milk is in the separator

--- src/main/java/net/mod1/Separator.java
+++ src/main/java/net/mod1/Separator.java
@@ -11,6 +11,8 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -20,14 +22,22 @@ import net.minecraft.world.World;
 
 public class Separator extends Block implements BlockEntityProvider {
 
+    public static final BooleanProperty HASMILK = BooleanProperty.of("hasmilk");
+
     public Separator() {
         super(FabricBlockSettings.of(Material.METAL).nonOpaque().breakByHand(true).breakByTool(FabricToolTags.PICKAXES, 0)
                 .sounds(BlockSoundGroup.ANVIL).strength(4.0f, 35.0f).build());
+            setDefaultState(getStateManager().getDefaultState().with(HASMILK, false));
     }
 
     @Override
     public BlockEntity createBlockEntity(BlockView blockView) {
        return new SeparatorEntity();
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
+        stateManager.add(HASMILK);
     }
 
     @Override
@@ -49,6 +59,7 @@ public class Separator extends Block implements BlockEntityProvider {
             if(player.getStackInHand(hand).getItem() == Items.MILK_BUCKET){
                 if(separatorEntity.addMilk()){
                     player.setStackInHand(hand, new ItemStack(Items.BUCKET));
+                    world.setBlockState(pos, Mod1.SEPARATOR.getDefaultState().with(HASMILK, true));
                     return ActionResult.SUCCESS;
                 }
             }
@@ -56,6 +67,7 @@ public class Separator extends Block implements BlockEntityProvider {
                 if(separatorEntity.removeMilk()){
                     player.getStackInHand(hand).decrement(1);
                     player.giveItemStack(new ItemStack(Items.MILK_BUCKET));
+                    world.setBlockState(pos, Mod1.SEPARATOR.getDefaultState().with(HASMILK, false));
                     return ActionResult.SUCCESS;
                 }
                 return ActionResult.FAIL;
